ca1961b2b7fc767eb74358a9580d709dcaef6882
mousa92 <47175383+mousa92@users.noreply.github.com>
2019-01-30 15:11:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,41 @@
 <?php
+function gradingStudents(...$grades){
+ 
 
-$handle = fopen ("php://stdin","r");
-fscanf($handle,"%d",$n);
-
-for($a0 = 0; $a0 < $n; $a0++){
-fscanf($handle,"%d",$grade);
-
-$next = round($grade/5) * 5;
-$condition = $next - $grade < 3;
-
-if ( $next > $grade && is_float($grade / 5) && $condition && $grade > 37) {
-if (!$condition) {
-echo $grade."\n";
-}else{
-echo $next."\n";
-}
-}else{
-echo $grade."\n";
-}
+ echo "Sample Input O <br>";
+ for ($i=0;$i <= sizeof($grades); $i++){
+    
+    
+	echo $grades[$i];
+    echo "<br/>";
 }
 
+echo "Sample Output O <br>";
+for ($i=0;$i <= sizeof($grades); $i++){
+    if ($grades[$i] >= 38){
+        if ($grades[$i] % 5 == 3){
+            $grades[$i] += 2;
+        }
+        else if ($grades[$i] % 5 == 4){
+            $grades[$i] += 1;
+        }
+    }
+    
+    
+	echo $grades[$i];
+    echo "<br/>";
+}    
+}
+
+gradingStudents(73,67,38,33); 
+ 
 ?>
+
+
+
+
+
+ 
+
+
+
